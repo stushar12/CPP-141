30206932555602766c5c2db99e29795780aa2371
Tushar Saxena <stushar345@gmail.com>
2021-02-27 17:32:24 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,25 +1,35 @@
- string secFrequent (string seq[], int n)
+#include<bits/stdc++.h>
+using namespace std;
+
+string secondmost(string str[], int n)
     {
-        unordered_map<string, int> occ; 
+        unordered_map<string, int> m; 
     for (int i = 0; i < n; i++) 
-        occ[seq[i]]++; 
+        m[str[i]]++; 
         
         int first_max = INT_MIN, sec_max = INT_MIN; 
-    for (auto it = occ.begin(); it != occ.end(); it++) { 
-        if (it->second > first_max) { 
+    for (auto it = m.begin(); it != m.end(); it++) 
+    { 
+        if (it->second > first_max) 
+        { 
             sec_max = first_max; 
             first_max = it->second; 
-        } 
-  
-        else if (it->second > sec_max &&  
-                 it->second != first_max) 
+        }
+        else if (it->second > sec_max &&  it->second != first_max) 
             sec_max = it->second; 
     } 
-  
-    // Return string with occurrence equals 
-    // to sec_max 
-    for (auto it = occ.begin(); it != occ.end(); it++) 
+   
+    for (auto it = m.begin(); it != m.end(); it++)
+    { 
         if (it->second == sec_max) 
             return it->first; 
-
     }
+    return "";
+    }
+
+int main()
+{
+    string str[]={"geek", "for", "geek", "for", "geek", "aaa"};
+    int n=sizeof(str)/sizeof(str[0]);
+    cout<<secondmost(str,n);
+}
